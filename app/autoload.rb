9b071f45855f8cb94d924ff7aa308272a36fb4af
V. Stoyanov <devphp.web@gmail.com>
2016-01-16 13:27:33 +0200

    Implemented an autoloader for routes.

--- app/autoload.rb
+++ app/autoload.rb
@@ -4,3 +4,10 @@ Dir[models_dir].each do |model|
   model_name = File.basename(model, '.rb')
   autoload ActiveSupport::Inflector.camelize(model_name), model
 end
+
+# Now let's load all our routes (controllers).
+routes_dir = File.join(File.expand_path('../routes', __FILE__), '*.rb')
+Dir[routes_dir].each do |route|
+  route_name = File.basename(route, '.rb')
+  autoload ActiveSupport::Inflector.camelize(route_name), route
+end
