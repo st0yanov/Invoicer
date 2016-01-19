b1870dad7eea11c7ea172e082a51db64b434f9da
V. Stoyanov <devphp.web@gmail.com>
2016-01-19 15:36:02 +0200

    Created the structure for the #users table.

--- db/migrate/20160119125744_create_users_table.rb
+++ db/migrate/20160119125744_create_users_table.rb
@@ -1,4 +1,11 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username, limit: 32
+      t.string :password
+      t.boolean :active
+      t.integer :user_level, limit: 2
+      t.string :last_ip, limit: 15
+    end
   end
 end
