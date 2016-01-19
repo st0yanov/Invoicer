bfe776b72b08801b7b1ab9e2d530d72c39f38444
V. Stoyanov <devphp.web@gmail.com>
2016-01-19 14:40:16 +0200

    Added database configuration depending on environment.

--- app/config/database.rb
+++ app/config/database.rb
@@ -1,16 +1,18 @@
 # This file contains all the database related settings.
 
 configure do
-  # Log queries to STDOUT in development
-  if settings.development?
+  # Log queries to STDOUT in development and use sqlite database.
+  if settings.development? or settings.test?
+    set :database, { adapter: "sqlite3", database: "database.sqlite3" }
     ActiveRecord::Base.logger = Logger.new(STDOUT)
+  else
+    set :database, {
+                      adapter: 'mysql2',
+                      host:     ENV['DATABASE_SERVER'],
+                      username: ENV['DATABASE_USER'],
+                      password: ENV['DATABASE_PASSWORD'],
+                      database: ENV['DATABASE_NAME']
+                   }
   end
 
-  set :database, {
-                  adapter: 'mysql2',
-                  host:     ENV['DATABASE_SERVER'],
-                  username: ENV['DATABASE_USER'],
-                  password: ENV['DATABASE_PASSWORD'],
-                  database: ENV['DATABASE_NAME']
-                }
 end
