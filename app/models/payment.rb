48f825c3892abb68d3ace69f4a25b298883e4184
V. Stoyanov <devphp.web@gmail.com>
2016-02-12 15:07:09 +0200

    Added validation for payments.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,23 @@
 class Payment < ActiveRecord::Base
   belongs_to :invoice
+
+  validates :gateway, :date, :transaction, :value, presence: {
+    message: I18n.t('validation.presence')
+  }
+
+  validates :gateway, length: {
+    in: 2..32,
+    too_short: I18n.t('validation.length.too_short'),
+    too_long: I18n.t('validation.length.too_long')
+  }
+
+  validates :transaction, length: {
+    in: 1..64,
+    too_short: I18n.t('validation.length.too_short'),
+    too_long: I18n.t('validation.length.too_long')
+  }
+
+  validates :value, numericality: {
+    message: I18n.t('validation.value.numericality')
+  }
 end
--- db/migrate/20160212125004_create_payments_table.rb
+++ db/migrate/20160212125004_create_payments_table.rb
@@ -2,9 +2,9 @@ class CreatePaymentsTable < ActiveRecord::Migration
   def change
     create_table :payments do |t|
       t.belongs_to :invoice, index: true
-      t.string :gateway, null: false
+      t.string :gateway, limit: 32, null: false
       t.date :date, null: false
-      t.string :transaction, null: false
+      t.string :transaction, limit: 64, null: false
       t.decimal :value, null: false
     end
   end
